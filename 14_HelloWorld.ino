2bb174da28b22bbe75b483c0f3d1b1bcb277e249
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-02-03 10:22:35 +0530

    Create 14_HelloWorld.ino

---
+++ 14_HelloWorld.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);   //Select output pin
+  Serial.begin(9600);
+  Serial.println("Hello World!");  //Print on serial monitor
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+}
